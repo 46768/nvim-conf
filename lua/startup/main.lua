3590d3a151a4cf0cee46515ace0d1cabca7bf685
46768 <46768@bodin.ac.th>
2024-10-19 22:24:13 +0700

    terminal should be a toggle

--- lua/startup/main.lua
+++ lua/startup/main.lua
@@ -1,11 +1,4 @@
 require("startup.terminal")
-require("startup.windows")
-
-local lineTotal = vim.o.lines-2
-local mainLines = math.floor(0.7*lineTotal)
-local termLines = lineTotal - mainLines
 
 vim.cmd.NvimTreeFocus()
 
-vim.api.nvim_set_current_win(1001)
-vim.cmd.resize(mainLines)
--- lua/startup/terminal.lua
+++ lua/startup/terminal.lua
@@ -1,9 +1,34 @@
-local fn = require("fn")
+local fn = require('fn')
 
-vim.cmd.terminal()
-if (fn.OS == "Windows_NT") then
-	vim.cmd.edit "term://powershell"
-elseif (fn.OS == "Linux") then
-	vim.cmd.edit "term://bash"
-end
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
+-- Store the terminal buffer and window globally
+local terminal_bufnr = nil
+local terminal_winid = nil
+
+-- Toggle function to show/hide the same terminal
+function ToggleTerminal()
+    if terminal_bufnr and vim.api.nvim_buf_is_valid(terminal_bufnr) then
+        -- If the terminal window is open, close it
+        if terminal_winid and vim.api.nvim_win_is_valid(terminal_winid) then
+            vim.api.nvim_win_hide(terminal_winid)
+            terminal_winid = nil
+        else
+            -- If the terminal buffer exists but is hidden, show it
+            vim.cmd("split")
+            terminal_winid = vim.api.nvim_get_current_win()
+            vim.api.nvim_win_set_buf(terminal_winid, terminal_bufnr)
+        end
+    else
+        -- If the terminal doesn't exist, create a new one
+		if fn.OS == "windows_NT" then
+			vim.cmd("split | term powershell")
+		else
+			vim.cmd("split | term bash")
+		end
+        terminal_bufnr = vim.api.nvim_get_current_buf()
+        terminal_winid = vim.api.nvim_get_current_win()
+    end
+end
+
+-- Keymap to toggle the terminal
+vim.api.nvim_set_keymap('n', '<leader>t', ':lua ToggleTerminal()<CR>', { noremap = true, silent = true })
