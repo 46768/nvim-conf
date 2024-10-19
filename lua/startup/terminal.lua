3f0821071b108765ffa3e1c801afead66c77b964
46768 <46768@bodin.ac.th>
2024-10-19 22:10:30 +0700

    terminal should use bash if linux and ps if windows

--- lua/startup/terminal.lua
+++ lua/startup/terminal.lua
@@ -1,5 +1,9 @@
 local fn = require("fn")
 
 vim.cmd.terminal()
-vim.cmd.edit "term://bash"
+if (fn.OS == "Windows_NT") then
+	vim.cmd.edit "term://powershell"
+elseif (fn.OS == "Linux") then
+	vim.cmd.edit "term://bash"
+end
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
